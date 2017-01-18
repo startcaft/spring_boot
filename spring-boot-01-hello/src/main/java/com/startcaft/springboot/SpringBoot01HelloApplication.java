ba7695d364557e69bcd04baf409fb1661e3afa15
startcaft <startcaft@startcaft-PC>
2017-01-18 09:19:00 +0800

    Spring Boot项目的创建，以及核心注解 @SpringBootApplication 介绍，它是一个组合注解。

---
+++ spring-boot-01-hello/src/main/java/com/startcaft/springboot/SpringBoot01HelloApplication.java
@@ -0,0 +1,12 @@
+package com.startcaft.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot01HelloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot01HelloApplication.class, args);
+	}
+}
