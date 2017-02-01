e560b5a33f1ae7385bea0ce771ef61757c532c39
startcaft <51418996@qq.com>
2017-02-01 21:33:47 +0800

    spring boot 中使用 AOP，配合logback完成Web层的统一日志记录。

---
+++ springboot-aop-log/src/main/java/com/startcaft/www/SpringbootAopLogApplication.java
@@ -0,0 +1,12 @@
+package com.startcaft.www;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAopLogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootAopLogApplication.class, args);
+	}
+}
