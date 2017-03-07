92c3ef672bdd7f4d4c05630dbd719b7f03bceb3e
startcaft <startcaft@startcaft-PC>
2017-03-07 14:21:47 +0800

    分页查询相关

--- permission-core/src/main/java/com/permission/core/queryable/PageRequest.java
+++ permission-core/src/main/java/com/permission/core/queryable/PageRequest.java
@@ -8,8 +8,8 @@ public abstract class PageRequest {
 	
 	private int page = 0;								// 当前页，默认0
 	private int rows = 20;								// 每页显示记录数，默认20
-	private String sort = "id";							// 排序字段，默认"id"
-	private String order = "desc";						// 排序方式，asc/desc，默认"desc"
+	private String sortField = "id";					// 排序字段，默认"id"
+	private String orderDirection = "desc";				// 排序方向，asc/desc，默认"desc"
 	
 	public int getPage() {
 		return page;
@@ -23,16 +23,16 @@ public abstract class PageRequest {
 	public void setRows(int rows) {
 		this.rows = rows;
 	}
-	public String getSort() {
-		return sort;
+	public String getSortField() {
+		return sortField;
 	}
-	public void setSort(String sort) {
-		this.sort = sort;
+	public void setSortField(String sortField) {
+		this.sortField = sortField;
 	}
-	public String getOrder() {
-		return order;
+	public String getOrderDirection() {
+		return orderDirection;
 	}
-	public void setOrder(String order) {
-		this.order = order;
+	public void setOrderDirection(String orderDirection) {
+		this.orderDirection = orderDirection;
 	}
 }
