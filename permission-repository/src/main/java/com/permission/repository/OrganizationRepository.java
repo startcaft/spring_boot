3c95e107df984f713b709d91cf629a39d4fb9dfa
startcaft <startcaft@startcaft-PC>
2017-03-08 11:37:36 +0800

    添加Repository接口

--- permission-repository/src/main/java/com/permission/repository/OrganizationRepository.java
+++ permission-repository/src/main/java/com/permission/repository/OrganizationRepository.java
@@ -2,9 +2,13 @@ package com.permission.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
+
 import com.permission.core.entity.Organization;
 
 public interface OrganizationRepository extends JpaRepository<Organization, Integer>
 												,JpaSpecificationExecutor<Organization>{
-
+	
+	@Query("select org from Organization where org.name = ?")//name是唯一的
+	Organization queryByName(String orgName);
 }
