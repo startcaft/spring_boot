72395a6660c190926af3a47bcd7fc6cfd42efa16
startcaft <startcaft@startcaft-PC>
2017-03-10 16:48:28 +0800

    自定义异常

--- permission-core/src/main/java/com/permission/core/exception/RecordExistException.java
+++ permission-core/src/main/java/com/permission/core/exception/RecordExistException.java
@@ -10,7 +10,7 @@ public class RecordExistException extends Exception {
 	 */
 	private static final long serialVersionUID = 1L;
 
-	public RecordExistException(String EntityPropertyName) {
-		super("指定的" + EntityPropertyName + "已经存在");
+	public RecordExistException(String msg) {
+		super(msg);
 	}
 }
